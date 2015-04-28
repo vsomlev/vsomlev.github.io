e1f9a4c393e34d25ad099f102c576d8726c291dc
Vesso Somlev <v.somlev@gmail.com>
2015-04-28 19:38:38 +0000

    sync with project

--- loadout/gui_control.js
+++ loadout/gui_control.js
@@ -1,36 +1,40 @@
-function data_fetch(done_cb){
+function data_fetch(){
 	$.ajax({
 		url: 'items_data.json',
-		success: done_cb,
+		success: on_data_loaded,
 		error: function(x) {alert('Failed loading the items description. Refresh and try again.');}
 	});
 };
 
-function has_conflicts(itemA, itemB){
-	if(typeof itemA == 'undefined' || typeof itemB == 'undefined') return false;
+function on_data_loaded(data){
+	selected_items = [];
+	class_filter = 'all';
+	items_list = data;
+	$('#selector-table').show();
+	$('#loading-text').hide();
 
-	var eqa = itemA['e'];
-	var eqb = itemB['e'];
-    // intersection
-    return eqa.some(function(x){return eqb.indexOf(x)!=-1});
+	update_autocomplete();
 };
 
 function update_conflicts(){
 	$('#conflict-status').html('Checking...');
-	var conflict_status = '';
+	var conflict_status = "";
 
-	for(var i=0; i<selected_items.length; i++){
-		var itemA = selected_items[i];
-		for(var j=i+1; j<selected_items.length; j++){
-			var itemB = selected_items[j];
-			var conflict = has_conflicts(itemA, itemB);
-			if(conflict){
-				conflict_status += "Conflict between '" + itemA['name'] + "' and '" + itemB['name'] + "'.<br/>";
-			}
-		}
-	}
+    selected_items.forEach(function(itemA, i){
+        selected_items.slice(++i).forEach(function(itemB){
+            if(typeof itemA == 'undefined' || typeof itemB == 'undefined') return;
 
-	if(conflict_status.length==0){
+            var eqa = itemA['e'];
+            var eqb = itemB['e'];
+
+            // intersection of equip regions
+            if(eqa.some(function(x){return eqb.indexOf(x)!=-1})){
+                conflict_status += "Conflict between '" + itemA['name'] + "' and '" + itemB['name'] + "'.<br/>";
+            } 
+        });
+    });
+
+	if(conflict_status.trim().length==0){
 		$('#conflict-status').html("No conflicts!");
 	} else {
 		$('#conflict-status').html(conflict_status);
@@ -44,14 +48,35 @@ function on_item_select(event, ui){
     new_item['name'] = ui.item.value;
 	selected_items[item_idx-1] = new_item;
 
-    var item_image_name = new_item['i'];
+    change_item_image(item_idx, new_item);
+	update_conflicts();
+};
+
+// invoked when scrolling the suggestions with arrow up/down
+function on_item_preview(event, ui){
+    var item_idx = parseInt(event.target.id.split('-')[1]);
+	var new_item = items_list[ui.item.value];
+    change_item_image(item_idx, new_item);
+};
+
+// invoked when cancelling the autocomplete (ESC or deleting the text)
+function on_autocomplete_close(event, ui){
+    var item_idx = parseInt(event.target.id.split('-')[1]);
+    old_item = selected_items[item_idx-1];
+    if(typeof old_item === 'undefined'){
+        $("#item-"+item_idx+"-entry").val('');
+    } else {
+        $("#item-"+item_idx+"-entry").val(old_item['name']);
+    }
+    change_item_image(item_idx, old_item);
+};
+
+function change_item_image(item_idx, item){
     var item_image_url = 'empty.png';
-    if(item_image_name){
-        item_image_url = "http://media.steampowered.com/apps/440/icons/" + item_image_name;
+    if(item && item['i']){
+        item_image_url = "http://media.steampowered.com/apps/440/icons/" + item['i'];
     }
 	$("#item-"+item_idx+"-image").attr('src', item_image_url);
-
-	update_conflicts();
 };
 
 function clear_item(event, ui){
@@ -65,6 +90,7 @@ function clear_item(event, ui){
 
 function update_autocomplete(){
 	var items_names_list = [];
+    
 	for(var name in items_list){
 		if(class_filter=='all' || items_list[name]['c'].length==0 || items_list[name]['c'].indexOf(class_filter)>-1){
 			items_names_list.push(name);
@@ -72,19 +98,11 @@ function update_autocomplete(){
 	}
 	var autocomplete_options = {
 		source: items_names_list,
-		select: on_item_select
+		select: on_item_select,
+        focus: on_item_preview, //scrolling through the suggestions
+        close: on_autocomplete_close // cancelling autocomplete
 	};
     $(".item-entry").autocomplete(autocomplete_options);
-};
-
-function on_data_loaded(data){
-	selected_items = [];
-	class_filter = 'all';
-	items_list = data;
-	$('#selector-table').show();
-	$('#loading-text').hide();
-
-	update_autocomplete();
 };
 
 function set_class_filter(event){
